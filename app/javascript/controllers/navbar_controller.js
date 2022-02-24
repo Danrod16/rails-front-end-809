1823ed42a6fa72671d98613e7ddfe942c9699726
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-02-24 10:58:02 +0800

    Git init: rails front end setup

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,6 +1,6 @@
-// Visit The Stimulus Handbook for more details 
+// Visit The Stimulus Handbook for more details
 // https://stimulusjs.org/handbook/introduction
-// 
+//
 // This example controller works with specially annotated HTML like:
 //
 // <div data-controller="hello">
@@ -10,9 +10,11 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
-
-  connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
-  }
+ updateNavbar() {
+    if (window.scrollY >= window.innerHeight) {
+      this.element.classList.add("navbar-lewagon-white")
+    } else {
+      this.element.classList.remove("navbar-lewagon-white")
+    }
+  };
 }
